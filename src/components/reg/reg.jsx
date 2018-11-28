e8e934bf49aa2edfde8770619272cae9f613e8f9
zzw <541938763@qq.com>
2018-11-28 21:58:14 +0800

    更新路由模板

--- src/components/reg/reg.jsx
+++ src/components/reg/reg.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 
+import {Button} from 'antd-mobile';
+
 class Reg extends Component {
     render() {
         return (
             <div>
                 注册
+                <Button className="btn" type="primary">primary 按钮</Button>
             </div>
         );
     }
